87610941ac65b632b0418ab41147a2822c5b7cfa
Wygeass <48532140@qq.com>
2017-03-31 14:26:33 +0800

    Initial Commit

---
+++ LearnSystem/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LearnSystem
+//
+//  Created by Wygeass on 2017/3/31.
+//  Copyright © 2017年 Wygeass. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
